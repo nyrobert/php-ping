734cae1cd5d6c085ccb630d383215438ffae5056
robi <nyitrai.robert@ustream.tv>
2017-02-02 12:34:29 +0100

    update output

--- ping.php
+++ ping.php
@@ -10,22 +10,29 @@ $socket = socket_create(AF_INET, SOCK_RAW, $protocolNumber);
 socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec' => TIMEOUT, 'usec' => 0));
 socket_connect($socket, HOST, 0);
 
-for ($i = 1; $i <= COUNT; $i++) {
+echo sprintf('PING %s (%s):', HOST, gethostbyname(HOST)) . PHP_EOL;
+
+for ($i = 0; $i < COUNT; $i++) {
 	$startTime = microtime(true);
 
 	$package  = "\x08\x00\x19\x2f\x00\x00\x00\x00\x70\x69\x6e\x67";
 	socket_send($socket, $package, strlen($package), 0);
 
 	if (socket_read($socket, 255)) {
-		echo formatTime(microtime(true) - $startTime) . PHP_EOL;
+		printLine($i, $startTime);
 	} else {
 		$result = false;
 	}
 }
 
+function printLine($seq, $startTime)
+{
+	echo sprintf('icmp_seq=%d time=%s', $seq, formatTime(microtime(true) - $startTime)) . PHP_EOL;
+}
+
 function formatTime($time)
 {
-	return round($time * 1000, 3) . ' ms';
+	return sprintf('%.3f ms', round($time * 1000, 3));
 }
 
 socket_close($socket);
