4abd389dcbe3f4c80a23e7ba120fd4d389958a28
robi <nyitrai.robert@ustream.tv>
2017-02-07 14:15:31 +0100

    add count, interval and destination command line options

--- README.md
+++ README.md
@@ -10,11 +10,27 @@ implemented using PHP's socket functions.
 
 ## Usage
 
+### Synopsis
+
+ping [-c *count*] [-i *interval*]  *destination*
+
+### Options
+
+-c *count*: Stop after sending *count* ECHO_REQUEST packets (default is 3).
+
+-i *interval*: Wait *interval* seconds between sending each packet (default is 1 second).
+
+## Examples
+
 Access to raw sockets on UNIX like systems requires root access.
 
  ```shell
-  sudo php ping.php
-  ```
+  sudo php ping.php google.com
+ ```
+ 
+ ```shell
+  sudo php ping.php -c 10 -i 2 google.com
+ ```
 
 ## License
 
--- ping.php
+++ ping.php
@@ -1,17 +1,14 @@
 #!/usr/bin/env php
 <?php
 
-const HOST     = 'index.hu';
 const PROTOCOL = 'icmp';
-const COUNT    = 3;
-const TIMEOUT  = 1;
 
 function ping()
 {
 	$ipAddress      = getIpAddress();
 	$protocolNumber = getprotobyname(PROTOCOL);
 	$socket         = socket_create(AF_INET, SOCK_RAW, $protocolNumber);
-	socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec' => TIMEOUT, 'usec' => 0));
+	socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec' => 1, 'usec' => 0));
 	socket_connect($socket, HOST, 0);
 
 	echo sprintf('PING %s (%s):', HOST, $ipAddress) . PHP_EOL;
@@ -27,14 +24,35 @@ function ping()
 		} else {
 			echo 'Request timed out.';
 		}
+
+		if ($i < (COUNT - 1)) {
+			sleep(INTERVAL);
+		}
 	}
 
 	socket_close($socket);
 }
 
+function parseOptions($argv)
+{
+	$options = getopt('c:i:');
+
+	define('COUNT', isset($options['c']) ? (int) $options['c'] : 3);
+	define('INTERVAL', isset($options['i']) ? (int) $options['i'] : 1);
+
+	$host = array_pop($argv);
+	if (count($argv) === 0 || empty($host)) {
+		echo 'ping: empty host' . PHP_EOL;
+		exit();
+	}
+
+	define('HOST', $host);
+}
+
 function getIpAddress()
 {
 	$ipAddress = gethostbyname(HOST);
+	var_dump($ipAddress);
 	if ($ipAddress === HOST) {
 		echo sprintf('ping: cannot resolve %s: Unknown host', HOST) . PHP_EOL;
 		exit();
@@ -53,4 +71,5 @@ function formatTime($time)
 	return sprintf('%.3f ms', round($time * 1000, 3));
 }
 
+parseOptions($argv);
 ping();
